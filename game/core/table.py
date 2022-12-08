6d6007570dd8623b936dafc9e7d1df0631f74ca4
Andrew <qwqwqww505@yahoo.com>
2022-12-08 14:40:00 +0200

    add table

---
+++ game/core/table.py
@@ -0,0 +1,33 @@
+import copy
+from enum import Enum
+from cell import Cell, CellName
+
+
+class TableSize(Enum):
+    DEFAULT = 9
+
+
+class Table:
+    def __init__(self, size: TableSize):
+        self._table: list[Cell] = []
+        self.size: TableSize = size
+
+        self._create_table(size)
+
+    def _create_table(self, size):
+        if not isinstance(size, TableSize):
+            raise ValueError('You args not support. Send only TableSize arg')
+
+        for i in range(size.value):
+            self._table.append(Cell())
+
+    def get_table(self):
+        return copy.deepcopy(self._table)
+
+    def set_symbol_cell(self, index: int, symbol: CellName):
+        size: int = self.size.value
+
+        if index > size:
+            raise IndexError(f'Index cell > size this table. {index} > {self.size.value}')
+
+        self._table[index].symbol = symbol
