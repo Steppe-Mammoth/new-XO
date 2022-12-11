4dda073cef5819f08e21782f7bc458020f027ea3
Andrew <qwqwqww505@yahoo.com>
2022-12-11 20:10:12 +0200

    Add checker to player symbol

--- game/core/player.py
+++ game/core/player.py
@@ -1,8 +1,15 @@
-from cell import Symbol
+from game.core.symbol import Symbol, symbol_check
 
 
 class Player:
+    def __new__(cls, name, symbol):
+        symbol_check(symbol)
+        instance = super().__new__(cls)
+        return instance
+
     def __init__(self, name: str, symbol: Symbol):
         self.name = name
         self.symbol = symbol
+        self.count_steps = 0
+
 
