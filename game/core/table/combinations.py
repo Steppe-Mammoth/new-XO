d7f20e3db768d591eaed59c76ab16b30811ed9a2
Andrew <qwqwqww505@yahoo.com>
2022-12-23 19:27:40 +0200

    Rename args

--- game/core/table/combinations.py
+++ game/core/table/combinations.py
@@ -2,11 +2,11 @@ from enum import verify, UNIQUE, Enum
 
 
 @verify(UNIQUE)
-class Cases(Enum):
-    CASE_RIGHT = 'index_row, index_cell + temp_index'
-    CASE_DOWN = 'index_row + temp_index, index_cell'
-    CASE_DOWN_RIGHT = 'index_row + temp_index, index_cell + temp_index'
-    CASE_DOWN_LEFT = 'index_row + temp_index, index_cell - temp_index'
+class Vectors(Enum):
+    RIGHT = 'index_row, index_cell + temp_index'
+    DOWN = 'index_row + temp_index, index_cell'
+    DOWN_RIGHT = 'index_row + temp_index, index_cell + temp_index'
+    DOWN_LEFT = 'index_row + temp_index, index_cell - temp_index'
 
 
 class Combinations:
@@ -17,8 +17,8 @@ class Combinations:
 
         for index_row in range(size_row):
             for index_cell in range(size_column):
-                for case in Cases:
-                    comb = cls.get_combination_from_cell(case=case,
+                for vector in Vectors:
+                    comb = cls.get_combination_from_cell(vector=vector,
                                                          step=(index_row, index_cell),
                                                          size_row=size_row,
                                                          size_column=size_column,
@@ -30,20 +30,20 @@ class Combinations:
 
     @classmethod
     def get_combination_from_cell(cls,
-                                  case: Cases,
+                                  vector: Vectors,
                                   step: tuple[int, int],
                                   size_row: int,
                                   size_column: int,
                                   size_combination: int):
         combination = []
 
-        index_row, index_cell = step  # args for eval(case.value)
+        index_row, index_cell = step  # args for eval
         temp_index = size_combination - 1
-        expected_index_row, expected_index_cell = eval(case.value)
+        expected_index_row, expected_index_cell = eval(vector.value)
 
         if (0 <= expected_index_row <= size_row - 1) and (0 <= expected_index_cell <= size_column - 1):
 
             for temp_index in range(size_combination):
-                combination.append(eval(case.value))
+                combination.append(eval(vector.value))
 
             return tuple(combination)
