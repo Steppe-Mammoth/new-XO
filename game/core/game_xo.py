05fcbc3bb965ce398ef751c964264bd04e5409be
Andrew <qwqwqww505@yahoo.com>
2023-01-18 16:47:31 +0200

    Reworked class interface GameBase, added checks of passed arguments Added standard implementation of class methods (partially transferred from standard implementation of Game class)

--- game/core/game_xo.py
+++ game/core/game_xo.py
@@ -1,87 +1,76 @@
-from dataclasses import dataclass
-from enum import Enum
-from typing import NamedTuple, Optional, Tuple
-from abc import ABC, abstractmethod
+from typing import Optional
 
-from game.core.cheker import CheckerDefault, CheckerBase
+from game.core.cheker import CheckerDefault, CheckerBase, check_checker_instance
 from game.core.players.player import PlayerBase
-from game.core.players.players import Players
+from game.core.players.players import PlayersBase, check_players_instance
+from game.core.result import ResultCode, ResultBase, Result
 from game.core.table.annotations import CombType
-from game.core.table.table import TableBase
+from game.core.table.table import TableBase, check_table_instance
 
 
-class ResultCode(Enum):
-    NO_RESULT = 0
-    ALL_CELLS_USED = 1
-    WINNER = 2
+class GameBase:
+    def __init__(self, players: PlayersBase, table: TableBase, checker: CheckerBase):
+        check_players_instance(players)
+        check_table_instance(table)
+        check_checker_instance(checker)
 
-
-class WinnerInfo(NamedTuple):
-    player: PlayerBase
-    combinations: CombType
-
-
-@dataclass(frozen=True)
-class Result:
-    code: ResultCode
-    value: Optional[WinnerInfo] = None
-
-    def __post_init__(self):
-        if type(self.code) is not ResultCode:
-            raise AttributeError('Only ResultsCode object ')
-
-        if self.code == ResultCode.WINNER:
-            if not isinstance(self.value, WinnerInfo):
-                raise AttributeError('WinnerInfo only')
-
-
-class GameBase(ABC):
-    def __init__(self, players: Players, checker: CheckerBase, table: TableBase):
-        self.checker = checker
-        self.players = players
-        self.table = table
-
-        self._result_game: Optional[Result] = None
+        self._players = players
+        self._table = table
+        self._checker = checker
+        self._game_result = Result(code=ResultCode.NO_RESULT)
 
     @property
-    def result_game(self) -> Result:
-        return ResultCode.NO_RESULT if not self._result_game else self._result_game
+    def players(self):
+        return self._players
 
-    @abstractmethod
+    @property
+    def table(self):
+        return self._table
+
+    @property
+    def game_result(self) -> ResultBase:
+        return self._game_result
+
     def step(self, index_row: int, index_column: int, player: PlayerBase):
         self.table.set_symbol_cell(index_row=index_row, index_column=index_column, symbol=player.symbol)
 
-    @abstractmethod
-    def result(self, player: PlayerBase) -> Tuple[Tuple[int, int]] | ResultCode:
-        return self.checker.result_player(symbol=player.symbol,
-                                          table=self.table.table,
-                                          combinations=self.table.combinations)
+    def result(self, player: PlayerBase) -> ResultCode:
+        if win_comb := self._player_result(player=player):
+            self._set_winner(player=player, win_combination=win_comb)
+
+        elif self._table_result():
+            self._set_table_complete()
+
+        return self.game_result.code
+
+    def _player_result(self, player: PlayerBase) -> Optional[CombType]:
+        return self._checker.result_player(player.symbol, table=self.table.table, combinations=self.table.combinations)
+
+    def _table_result(self) -> bool:
+        if self.table.count_free_cells == 0:
+            return True
+
+    def _set_winner(self, player: PlayerBase, win_combination: CombType):
+        self.game_result.update(code=ResultCode.WINNER, win_player=player, win_combination=win_combination)
+
+    def _set_table_complete(self):
+        self.game_result.update(code=ResultCode.ALL_CELLS_USED)
 
 
 class Game(GameBase):
-    def __init__(self, players: Players, table: TableBase, checker: CheckerBase = CheckerDefault()):
-        super().__init__(players, checker, table)
+    def __init__(self, players: PlayersBase, table: TableBase, checker: CheckerBase = CheckerDefault()):
+        super().__init__(players=players, table=table, checker=checker)
 
     def step(self, index_row: int, index_column: int, player: PlayerBase):
         super().step(index_row=index_row, index_column=index_column, player=player)
         player.add_count_step()
 
     def result(self, player: PlayerBase) -> ResultCode:
-        result_code = ResultCode.NO_RESULT
+        result = ResultCode.NO_RESULT
 
         if self.table.param.COMBINATION <= player.count_steps:
-            print(f"CHECK FOR {player.name=} | count_steps: {player.count_steps}")
-
-            if win_comb := super().result(player=player):
-                result_code = ResultCode.WINNER
-                result = Result(result_code, value=WinnerInfo(player=player, combinations=win_comb))
-                self._result_game = result
-
-            elif self.table.count_free_cells == 0:
-                result_code = ResultCode.ALL_CELLS_USED
-                self._result_game = Result(result_code)
-
-        return result_code
+            result = super().result(player=player)
+        return result
 
     def step_result(self, index_row: int, index_column: int, player: PlayerBase) -> ResultCode:
         self.step(index_row=index_row, index_column=index_column, player=player)
