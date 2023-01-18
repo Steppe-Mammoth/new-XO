f5f0a5689ea9eaf938511ecfa25f23e2fdbab156
Andrew <qwqwqww505@yahoo.com>
2023-01-18 16:40:16 +0200

    In interface class CellBase added standard validation of passed argument symbol

--- game/core/table/cell.py
+++ game/core/table/cell.py
@@ -1,23 +1,19 @@
 from abc import ABC
 
-from game.core.symbol import Symbol
-from game.exceptions.core_exceptions import SymbolError
+from game.core.symbol import Symbol, check_symbol
 
 
 class CellBase(ABC):
     __slots__ = "_symbol",
 
-    def __init__(self, symbol):
+    def __init__(self, symbol: Symbol):
+        check_symbol(symbol)
         self._symbol = symbol
 
     @property
-    def symbol(self):
+    def symbol(self) -> Symbol:
         return self._symbol
 
 
 class Cell(CellBase):
-    def __init__(self, symbol: Symbol):
-        if not isinstance(symbol, Symbol):
-            raise SymbolError
-
-        super().__init__(symbol)
+    pass
