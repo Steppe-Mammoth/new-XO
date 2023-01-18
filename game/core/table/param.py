2e99d28fa92b34a7219c22d0c90eaee1919a1de8
Andrew <qwqwqww505@yahoo.com>
2023-01-18 16:48:50 +0200

    Added verified func for TableParam instance

--- game/core/table/param.py
+++ game/core/table/param.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 
+from game.exceptions.core_exceptions import TableParamInstanceError
+
 
 @dataclass(frozen=True)
 class TableParam:
@@ -10,3 +12,8 @@ class TableParam:
     def __post_init__(self):
         if self.ROW < self.COMBINATION or self.COLUMN < self.COMBINATION:
             raise ValueError("ROW and COLUMN size must not be less than COMBINATION")
+
+
+def check_table_param_instance(param: TableParam):
+    if not isinstance(param, TableParam):
+        raise TableParamInstanceError
