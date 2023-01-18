af1534d6d157c60a40afd48d598d7ad1f36ad1c4
Andrew <qwqwqww505@yahoo.com>
2023-01-18 16:52:55 +0200

    Added default verified list players to class interface PlayersBase. Added verified func for PlayersBase instance

--- game/core/players/players.py
+++ game/core/players/players.py
@@ -1,21 +1,22 @@
 from abc import ABC, abstractmethod
-from typing import Iterable
+from typing import Optional, Sequence, Generator
 
-from game.core.players.player import PlayerBase, Player
-from game.core.symbol import Symbol
+from game.core.players.player import PlayerBase, CheckPlayers
+from game.exceptions.core_exceptions import PlayersInstanceError
 
 
 class PlayersBase(ABC):
     __slots__ = '_players', '_now_player'
 
-    def __init__(self, players: Iterable[PlayerBase]):
-        self._players = []
-        self._now_player: PlayerBase | None = None
-        self.add_players(players)
+    def __init__(self, players: Sequence[PlayerBase]):
+        CheckPlayers.list_players(players=players)
 
-    @abstractmethod
-    def add_players(self, players: Iterable[PlayerBase]):  # add test
-        self._players += players
+        self._players = players
+        self._now_player: Optional[PlayerBase] = None
+
+    def set_now_player(self, player: PlayerBase):  # add test
+        CheckPlayers.player_instance(player=player)
+        self._now_player = player
 
     @property
     def players(self):
@@ -24,60 +25,38 @@ class PlayersBase(ABC):
     @property
     @abstractmethod
     def now_player(self) -> PlayerBase:
-        pass
+        return self._now_player
 
     @abstractmethod
     def get_next_player(self) -> PlayerBase:  # add test
         pass
 
-    @abstractmethod
-    def set_now_player(self, player: PlayerBase):  # add test
-        self._now_player = player
-
 
 class Players(PlayersBase):
-    def __init__(self, players: Iterable[PlayerBase]):
+    def __init__(self, players: Sequence[PlayerBase]):
         super().__init__(players)
-        self._deque_p = None
 
-    def add_players(self, players: Iterable[PlayerBase]):  # Добавить списком
-        for player in players:
-            if not isinstance(player, PlayerBase):
-                raise ValueError('Only Player')  # Зробить виключення
-        super().add_players(players)
+        self._deque_players = None
 
     @property
     def now_player(self) -> PlayerBase:
         if not self._now_player:
-            if len(self._players) > 0:
-                self.set_now_player(self._players[0])
-            else:
-                raise AttributeError("First added players. self.players is empty")  # Зробить виключення 1
+            super().set_now_player(self._players[0])
         return self._now_player
 
-    def set_now_player(self, player: PlayerBase):
-        if not isinstance(player, PlayerBase):
-            raise ValueError('Only Player')  # Зробить виключення
-
-        super().set_now_player(player)
-
     def get_next_player(self) -> PlayerBase:
-        if len(self._players) == 0:
-            raise AttributeError('First added players. self.players is empty')  # Зробить виключення 2  # add test
+        if not self._deque_players:
+            self._deque_players = self._deque_for_players()
+            self._deque_players.send(None)
 
-        if not self._deque_p:
-            self._deque_p = self._deque_players()
-            self._deque_p.send(None)
+        return next(self._deque_players)
 
-        return next(self._deque_p)
-
-    def _deque_players(self):
+    def _deque_for_players(self) -> Generator:
         while True:
             for player in self._players:
                 yield player
 
 
-p1 = Player('Igor1', Symbol('X', 1))
-p2 = Player('Igor2', Symbol('O', 2))
-
-p = Players(players=(p1, p2))
+def check_players_instance(players: PlayersBase):
+    if not isinstance(players, PlayersBase):
+        raise PlayersInstanceError
