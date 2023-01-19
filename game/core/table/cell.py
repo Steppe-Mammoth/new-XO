d4f9d4cd78d4f1203ff0276617f926a462a51936
Andrew <qwqwqww505@yahoo.com>
2023-01-19 03:45:55 +0200

    Type annotation replacement

--- game/core/table/cell.py
+++ game/core/table/cell.py
@@ -1,17 +1,17 @@
 from abc import ABC
 
-from game.core.symbol import Symbol, check_symbol
+from game.core.symbol import SymbolBase, check_symbol
 
 
 class CellBase(ABC):
     __slots__ = "_symbol",
 
-    def __init__(self, symbol: Symbol):
+    def __init__(self, symbol: SymbolBase):
         check_symbol(symbol)
         self._symbol = symbol
 
     @property
-    def symbol(self) -> Symbol:
+    def symbol(self) -> SymbolBase:
         return self._symbol
 
 
