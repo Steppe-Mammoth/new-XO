2eefa08d3b493c6a3b2b8610ac4a6d806a5ee54b
Andrew <qwqwqww505@yahoo.com>
2023-01-20 07:02:20 +0200

    Rename import Symbol

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from game import TableParam, TableDefault, GameConsole, Player, Players, SymbolBase
+from game import TableParam, TableDefault, GameConsole, Player, Players, Symbol
 
-p1 = Player(name="ANDROID:1", symbol=SymbolBase('X'), role=Player.Role.ANDROID)
-p2 = Player(name="ANDROID:4", symbol=SymbolBase('I'), role=Player.Role.ANDROID)
-p3 = Player(name="ANDROID:3", symbol=SymbolBase('K'), role=Player.Role.ANDROID)
+p1 = Player(name="ANDROID:1", symbol=Symbol('X'), role=Player.Role.ANDROID)
+p2 = Player(name="ANDROID:4", symbol=Symbol('I'), role=Player.Role.ANDROID)
+p3 = Player(name="ANDROID:3", symbol=Symbol('K'), role=Player.Role.ANDROID)
 
-p4 = Player(name="PLAYER:2", symbol=SymbolBase('O'), role=Player.Role.USER)
+p4 = Player(name="PLAYER:2", symbol=Symbol('O'), role=Player.Role.USER)
 
 players = Players(players=[p1, p2, p3, p4])
-table = TableDefault(param=TableParam(5, 5, 4))
+table = TableDefault(param=TableParam(10, 10, 7))
 
 game = GameConsole(players=players, table=table)
 game.start_game()
