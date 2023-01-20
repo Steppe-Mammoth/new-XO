b02af396558c579683b539a23732a683f91c427c
Andrew <qwqwqww505@yahoo.com>
2023-01-20 06:57:43 +0200

    Added interface

--- game/core/symbol.py
+++ game/core/symbol.py
@@ -1,13 +1,19 @@
+from abc import ABC
+
 from game.exceptions.core_exceptions import SymbolError
 
 
-class SymbolBase:
+class SymbolBase(ABC):
     __slots__ = "name"
 
     def __init__(self, name: str):
         self.name = name
 
 
-def check_symbol(symbol):
+class Symbol(SymbolBase):
+    ...
+
+
+def verify_symbol(symbol):
     if not isinstance(symbol, SymbolBase):
         raise SymbolError
