e3a215801f12a8fa864923728213af74e61e05e7
Andrew <qwqwqww505@yahoo.com>
2023-01-20 07:00:17 +0200

    Reworked

--- game/tests/test_cell.py
+++ game/tests/test_cell.py
@@ -1,23 +1,16 @@
 import pytest
 from game.core.table.cell import Cell
-from game.core.symbol import Symbol
 from game.exceptions.core_exceptions import SymbolError
+from game.core.symbol import Symbol
 
 
-def test_set_cell_value_error():
-    with pytest.raises(SymbolError):
-        Cell("0")
-
-    with pytest.raises(SymbolError):
-        Cell(3)
-
-    with pytest.raises(SymbolError):
-        Cell(Symbol)
+def test_init_cell_true():
+    for symbol_name in ['X', 'QWQ', 1, 2, [234]]:
+        symbol = Symbol(name=symbol_name)
+        cell = Cell(symbol=symbol)
 
 
-def test_set_cell_true():
-    cell = Cell(Symbol.O)
-    assert cell.symbol in Symbol
-
-    cell = Cell(Symbol.X)
-    assert cell.symbol == Symbol.X
+def test_init_cell_error():
+    for bad_symbol in ['X', 'QWQ', 1, 2, [234]]:
+        with pytest.raises(SymbolError):
+            cell = Cell(symbol=bad_symbol)
