5ed9fb2d164bc210ff566d8fdfa8589cf59e7b25
Andrew <qwqwqww505@yahoo.com>
2023-01-20 06:51:47 +0200

    SymbolBase -> Symbol

--- game/__init__.py
+++ game/__init__.py
@@ -2,7 +2,7 @@ from .core.AI import AIFindDefault
 from .core.cheker import CheckerDefault
 from .core.players.player import Player
 from .core.players.players import Players
-from .core.symbol import SymbolBase
+from .core.symbol import Symbol
 from .core.table.table import TableDefault, TableParam
 from .core.table.cell import Cell
 from .core.game_xo import ResultCode, GameState, Game
