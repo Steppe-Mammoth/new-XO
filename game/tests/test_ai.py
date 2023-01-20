4882a345001fad6b8c90d9f4d7d55a5c7244ef2e
Andrew <qwqwqww505@yahoo.com>
2023-01-20 06:59:53 +0200

    Refactor

--- game/tests/test_ai.py
+++ game/tests/test_ai.py
@@ -3,12 +3,12 @@ from typing import Tuple
 from game.core.AI import AIFindDefault, AIFindBase
 from game.core.cheker import CheckerDefault, CheckerBase
 from game.core.players.player import Player
-from game.core.symbol import Symbol
+from game.core.symbol import SymbolBase
 from game.core.table.param import TableParam
 from game.core.table.table import TableDefault
 
-p_ai = Player(name='AI', symbol=Symbol(name='X', value=1))
-p_2 = Player(name='Petro', symbol=Symbol(name='O', value=2))
+p_ai = Player(name='AI', symbol=SymbolBase(name='X'))
+p_2 = Player(name='Petro', symbol=SymbolBase(name='O'))
 
 AI_FINDER: AIFindBase.__class__ = AIFindDefault
 CHECKER: CheckerBase.__class__ = CheckerDefault
@@ -21,21 +21,21 @@ def build_table():
 
 def test_ai_all_result_for_first_step():
     t = build_table()
-    ai_all_step = AI_FINDER._find_best_step(symbol=p_ai.symbol, table=t.table, combinations=t.combinations)
-    ai_get_step = AI_FINDER.get_best_step(symbol=p_ai.symbol, table=t.table, combinations=t.combinations)
-    assert ai_all_step == [list(comb) for comb in t.combinations]
+    ai_all_step = AI_FINDER._find_best_step(symbol=p_ai.symbol, table=t.game_table, combinations=t.combinations)
+    ai_get_step = AI_FINDER.get_step(symbol=p_ai.symbol, table=t.game_table, combinations=t.combinations)
+    assert ai_all_step.value == [list(comb) for comb in t.combinations]
 
-    assert len(ai_all_step) == 8
+    assert len(ai_all_step.value) == 8
     assert len(ai_get_step) == 2
 
 
 def test_ai_one_player_win_3():
     t = build_table()
     for i in range(3):
-        i_row, i_column = AI_FINDER.get_best_step(combinations=t.combinations, symbol=p_ai.symbol, table=t.table)
+        i_row, i_column = AI_FINDER.get_step(combinations=t.combinations, symbol=p_ai.symbol, table=t.game_table)
         t.set_symbol_cell(index_row=i_row, index_column=i_column, symbol=p_ai.symbol)
 
-    result = CHECKER.result_player(symbol=p_ai.symbol, table=t.table, combinations=t.combinations)
+    result = CHECKER.result_player(symbol=p_ai.symbol, table=t.game_table, combinations=t.combinations)
     print(result)
 
     assert isinstance(result, Tuple) and result in t.combinations
@@ -46,10 +46,10 @@ def test_ai_all_result_second_step():
     t.set_symbol_cell(index_row=2, index_column=0, symbol=p_2.symbol)
     t.set_symbol_cell(index_row=1, index_column=0, symbol=p_ai.symbol)  #
 
-    ai_all_step = AI_FINDER._find_best_step(symbol=p_ai.symbol, table=t.table, combinations=t.combinations)
-    ai_get_step = AI_FINDER.get_best_step(symbol=p_ai.symbol, table=t.table, combinations=t.combinations)
+    ai_all_step = AI_FINDER._find_best_step(symbol=p_ai.symbol, table=t.game_table, combinations=t.combinations)
+    ai_get_step = AI_FINDER.get_step(symbol=p_ai.symbol, table=t.game_table, combinations=t.combinations)
 
-    assert ai_all_step == [[(1, 1), (1, 2)]]
+    assert ai_all_step.value == [[(1, 1), (1, 2)]]
     assert ai_get_step in ((1, 1), (1, 2))
 
 
@@ -60,10 +60,10 @@ def test_ai_battle_1_fail_to_win_enemy_3():  # Перебити гравця н�
     t.set_symbol_cell(index_row=0, index_column=0, symbol=p_ai.symbol)  #
     t.set_symbol_cell(index_row=1, index_column=0, symbol=p_2.symbol)
 
-    i_row, i_column = AI_FINDER.get_best_step(symbol=p_ai.symbol, table=t.table, combinations=t.combinations)
+    i_row, i_column = AI_FINDER.get_step(symbol=p_ai.symbol, table=t.game_table, combinations=t.combinations)
     t.set_symbol_cell(index_row=i_row, index_column=i_column, symbol=p_ai.symbol)  #
 
-    assert t.table[1][1].symbol == p_ai.symbol
+    assert t.game_table[1][1].symbol == p_ai.symbol
 
 
 def test_ai_battle_2_fail_to_win_enemy_3():
@@ -73,5 +73,5 @@ def test_ai_battle_2_fail_to_win_enemy_3():
     t.set_symbol_cell(index_row=1, index_column=0, symbol=p_ai.symbol)  #
     t.set_symbol_cell(index_row=2, index_column=1, symbol=p_2.symbol)
 
-    ai_get_step = AI_FINDER.get_best_step(symbol=p_ai.symbol, table=t.table, combinations=t.combinations)
+    ai_get_step = AI_FINDER.get_step(symbol=p_ai.symbol, table=t.game_table, combinations=t.combinations)
     assert ai_get_step == (2, 2)
