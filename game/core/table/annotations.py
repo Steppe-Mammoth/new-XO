d9f36fdded57b1cd5cc755428b4fe63e6eb65a05
Andrew <qwqwqww505@yahoo.com>
2023-01-23 05:00:10 +0200

    Correcting in the descriptions

--- game/core/table/annotations.py
+++ game/core/table/annotations.py
@@ -13,8 +13,10 @@ Example: ((0, 1), ...)
 
 CombsType = tuple[CombType, ...]
 """
-CombsType = Tuple[CombType, ...]\n
-*CombType = Tuple[CellIndex, ...]\n
-**CellIndex = Tuple[index_row: int, index_column: int]\n
+CombsType = tuple[tuple[tuple[i_row: int, i_column: int], ...], ...] or\n
+tuple[CombType, ...]\n
+Where:\n
+*CombType = tuple[CellIndex, ...]\n
+**CellIndex = tuple[index_row: int, index_column: int]\n
 Example: ( ((0, 0), (0, 1), (0, 2)), ((1, 0), (1, 1), (1, 2)), ... )
 """
