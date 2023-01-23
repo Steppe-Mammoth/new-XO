950ce36fa3994eeff6d881e1409ef92a0ca1aae4
Andrew <qwqwqww505@yahoo.com>
2023-01-23 04:57:20 +0200

    Change base responsibility and reworked

--- game/core/table/table.py
+++ game/core/table/table.py
@@ -1,7 +1,7 @@
-from typing import Optional
+from typing import Optional, TypeVar
 from abc import ABC, abstractmethod
 
-from game.core.table.combinations import CombDefault, CombinationsBase, check_combinations_instance
+from game.core.table.combinations import CombDefault, verify_combs_list
 from game.core.table.param import TableParam, verify_table_param_instance
 from game.core.table.annotations import CombsType
 from game.core.table.cell import Cell
@@ -9,43 +9,43 @@ from game.core.table.cell import Cell
 from game.core.symbol import SymbolBase
 from game.exceptions.core_exceptions import CellAlreadyUsedError, TableIndexError, TableInstanceError, AllCellsUsedError
 
+ClearGameFieldType = tuple[list[None], ...]
+GameFieldType = tuple[list[Cell | None], ...]
+TableT = TypeVar('TableT', bound='TableBase', covariant=True)
 
-def create_2d_list(row: int, column: int) -> tuple[list[None], ...]:
-    return tuple([None for _ in range(row)] for _ in range(column))  # add instance checker
+
+def create_2d_list(row: int, column: int) -> ClearGameFieldType:
+    return tuple([None for _ in range(row)] for _ in range(column))
+
+
+def verify_game_table(game_table, size_row, size_column):
+    if not all((len(game_table) == size_row,
+                all([len(column) == size_column for column in game_table]))):
+        raise ValueError('VERIFY GAME TABLE')
 
 
 class TableBase(ABC):
-    __slots__ = "_param", "_game_table", "_combinations", "_count_free_cells"
-
-    def __init__(self, param: TableParam, combinations: CombinationsBase):
-        verify_table_param_instance(param)
-        check_combinations_instance(combinations)
-
+    def __init__(self, param: TableParam):
         self._param = param
-        self._count_free_cells: int = param.ROW * param.COLUMN
-        self._game_table = self._get_new_playing_field()
-        self._combinations = combinations.get_combinations(size_row=param.ROW,
-                                                           size_column=param.COLUMN,
-                                                           size_combination=param.COMBINATION)
 
     @property
     def param(self) -> TableParam:
         return self._param
 
-    @property  # Reassign for those who care about security, because modifiable objects are returned.
-    def game_table(self) -> tuple[list[Optional[Cell]], ...]:
-        return self._game_table
-
-    @property  # Reassign for those who care about security, because modifiable objects are returned.
-    def combinations(self) -> CombsType:
-        return self._combinations
+    @property
+    @abstractmethod
+    def game_field(self) -> GameFieldType:
+        ...
 
     @property
-    def count_free_cells(self):
-        return self._count_free_cells
+    @abstractmethod
+    def combinations(self) -> CombsType:
+        ...
 
-    def _remove_free_cell(self):
-        self._count_free_cells -= 1
+    @property
+    @abstractmethod
+    def count_free_cells(self) -> int:
+        ...
 
     @abstractmethod
     def set_symbol_cell(self, index_row: int, index_column: int, symbol: SymbolBase):
@@ -54,24 +54,59 @@ class TableBase(ABC):
         * Subtracts the number of free cells by -1 after successful installation.
         """
         ...
-        self._remove_free_cell()
 
     @abstractmethod
-    def _get_new_playing_field(self) -> tuple[list[None], ...]:
+    def _remove_free_cell(self):
         ...
 
 
 class TableDefault(TableBase):
+    __slots__ = "_param", "_game_field", "_combinations", "_count_free_cells"
 
-    def __init__(self, param: TableParam, combinations: CombinationsBase = CombDefault()):
-        super().__init__(param, combinations=combinations)
+    def __init__(self, param: TableParam, combinations_list: Optional[CombsType] = None):
+        """
+        :param param: Instance class TableParam
+        :param combinations_list: The list of winning combinations is used to
+          determine the winning cells for the players.
+          Each combination in the list must match the format:
+          combs_list[i_comb][i_cell][0]: int -> is index row, and
+          combs_list[i_comb][i_cell][1]: int -> is index column.
+          If not specified manually,
+          then the combinations will be calculated automatically according to the given values from the < self.param >
+        """
+        verify_table_param_instance(param)
+        if not combinations_list:
+            combinations_list: CombsType = CombDefault.get_combinations(size_row=param.ROW,
+                                                                        size_column=param.COLUMN,
+                                                                        size_combination=param.COMBINATION)
+        verify_combs_list(combinations_list)
+        super().__init__(param)
+
+        self._combinations = combinations_list
+        self._count_free_cells = param.ROW * param.COLUMN
+        self._game_field = create_2d_list(row=param.ROW, column=param.COLUMN)
+
+    @property
+    def game_field(self) -> GameFieldType:
+        return self._game_field
+
+    @property
+    def combinations(self) -> CombsType:
+        return self._combinations
+
+    @property
+    def count_free_cells(self) -> int:
+        return self._count_free_cells
+
+    def _remove_free_cell(self):
+        self._count_free_cells -= 1
 
     def set_symbol_cell(self, index_row: int, index_column: int, symbol: SymbolBase):
         if self.count_free_cells == 0:
-            raise AllCellsUsedError(game_table=self.game_table)
+            raise AllCellsUsedError(game_table=self.game_field)
 
         param = self.param
-        table = self.game_table
+        table = self.game_field
 
         if not (param.ROW - 1 >= index_row >= 0) or not (param.COLUMN - 1 >= index_column >= 0):
             raise TableIndexError(index_column, index_row=index_row, table_param=param)
@@ -81,9 +116,6 @@ class TableDefault(TableBase):
 
         table[index_row][index_column] = Cell(symbol=symbol)
         self._remove_free_cell()
-
-    def _get_new_playing_field(self) -> tuple[list[None], ...]:
-        return create_2d_list(row=self.param.ROW, column=self.param.COLUMN)
 
 
 def verify_table_instance(table: TableBase):
