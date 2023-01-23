46e9540653471084cb2c63a84af1f893a7ef48f7
Andrew <qwqwqww505@yahoo.com>
2023-01-23 04:58:21 +0200

    Change base responsibility Reworked Add methods for AI

--- game/core/game_xo.py
+++ game/core/game_xo.py
@@ -1,75 +1,93 @@
 from abc import abstractmethod
-from typing import Optional
+from typing import Optional, Type
 
-from game.core.cheker import CheckerDefault, CheckerBase, check_checker_instance
-from game.core.players.player import PlayerBase
-from game.core.players.players import PlayersBase, verify_players_instance
-from game.core.result import ResultCode, GameStateBase, GameState, GameStateT
-from game.core.table.annotations import CombType
-from game.core.table.table import TableBase, verify_table_instance
+from game.core.ai import AIBase, AIDefault
+from game.core.cheker import CheckerDefault, CheckerBase, verify_checker_instance
+from game.core.players.player import PlayerBase, PlayerT
+from game.core.players.players import verify_players_instance, PlayersT
+from game.core.result import ResultCode, GameState, GameStateT
+from game.core.table.annotations import CombType, CellIndex
+from game.core.table.table import verify_table_instance, GameFieldType, TableT
 
 
 class GameBase:
     def __init__(self,
-                 players: PlayersBase,
-                 table: TableBase,
-                 checker: CheckerBase,
-                 game_state: GameStateBase):
-        verify_players_instance(players)
-        verify_table_instance(table)
-        check_checker_instance(checker)
+                 players: PlayersT,
+                 table: TableT,
+                 game_state: GameStateT):
 
         self._players = players
         self._table = table
-        self._checker = checker
-        self._game_state: GameStateT = game_state
+        self._game_state = game_state
         """Game state. By default, it is indicated that the result is missing. changes as the game progresses"""
 
     @property
-    def game_table(self):
-        return self.table.game_table
-
-    @property
-    def current_player(self):
-        return self._players.current_player
-
-    @property
-    def players(self):
+    def players(self) -> PlayerT:
+        """Returns an instance of the Players class"""
         return self._players
 
     @property
-    def table(self):
+    def game_field(self) -> GameFieldType:
+        """Returns the playing field from a Table instance"""
+        return self.table.game_field
+
+    @property
+    def current_player(self) -> PlayerT:
+        return self._players.current_player
+
+    @property
+    def table(self) -> TableT:
         return self._table
 
     @property
-    def game_state(self):
+    def game_state(self) -> GameStateT:
         return self._game_state
 
+    @abstractmethod
     def step(self, index_row: int, index_column: int, player: PlayerBase):
         """
-        Places a player symbol on the playing field 'self.table'
-        * Added symbol in a table
+        Places a player symbol on the playing field 'self.table'\n
+        * Added symbol in a game field
         * Added +1 step in a player.count_step
-        * Added this player in current_player
         """
-        self.table.set_symbol_cell(index_row=index_row, index_column=index_column, symbol=player.symbol)
-        player.add_count_step()
+        ...
 
     @abstractmethod
     def result(self, player: PlayerBase) -> GameStateT:
         """
-        The function checks the cells of the player given in the argument and returns its result by possible categories:
-          - ResultCode.NO_RESULT \n
-          - ResultCode.ALL_CELLS_USED \n
-          - ResultCode.WINNER \n
-        If the ResultCode matches the logical end, calls methods to populate self.game_state. For example:
-          - ResultCode.WINNER → self.set_winner,
-          - ResultCode.ALL_CELLS_USED → self.set_draw
+        The function checks the cells for the player given in the argument and returns its game_state\n
+        The game_state matches the logical end when:\n
+        * game_state.code.WINNER + game_state.win_player and game_state.win_combination
+        * game_state.code.ALL_CELLS_USED
 
-        Returns ResultCode.
+        Returns self.game_state.
         """
         ...
 
+    @abstractmethod
+    def step_result(self, index_row: int, index_column: int, player: PlayerBase) -> GameStateT:
+        """
+        Sets the symbol for the given player by the given indices and returns its result \n
+        This is a unifying function. It includes:\n
+        * def self.step
+        * def self.result
+
+        Return: self.result: GameState.
+        """
+
+    @abstractmethod
+    def ai_get_step(self, player: PlayerBase) -> CellIndex:
+        ...
+
+    @abstractmethod
+    def ai_step(self, player: PlayerBase):
+        ...
+
+    @abstractmethod
+    def ai_step_result(self, player: PlayerBase) -> GameStateT:
+        ...
+
+    @abstractmethod
     def set_winner(self, player: PlayerBase, win_combination: CombType):
         """
         Changes the self._game_state parameter to the result of the game in which there is a winner. \n
@@ -78,8 +96,8 @@ class GameBase:
         * Add a reference to the winning player object \n
         * Add a player winning combination.
         """
-        self.game_state.update(code=ResultCode.WINNER, win_player=player, win_combination=win_combination)
 
+    @abstractmethod
     def set_draw(self):
         """
         Changes the self._game_state parameter to the result of a game in which all cells are used and
@@ -87,67 +105,75 @@ class GameBase:
         Changes: \n
         * Replaces the result code.
         """
-        self.game_state.update(code=ResultCode.ALL_CELLS_USED)
 
-    def _win_result(self, player: PlayerBase) -> Optional[CombType]:
-        return self._checker.result_player(player.symbol,
-                                           table=self.table.game_table,
-                                           combinations=self.table.combinations)
-
-    def _draw_result(self) -> bool:
-        if self.table.count_free_cells == 0:
-            return True
+    @abstractmethod
+    def set_get_next_player(self) -> PlayerT:
+        """
+        Replaces the current player with the next player in line and returns it.
+        New current player available via self.current_player
+        * this function is the same as self.table.set_get_next_player
+        """
 
 
 class Game(GameBase):
     def __init__(self,
-                 players: PlayersBase,
-                 table: TableBase,
-                 checker: CheckerBase = CheckerDefault(),
-                 game_state: GameStateBase = GameState()):
+                 players: PlayersT,
+                 table: TableT,
+                 game_state: GameStateT = GameState(),
+                 checker: Type[CheckerBase] | CheckerBase = CheckerDefault,
+                 ai: Type[AIBase] | AIBase = AIDefault):
 
-        super().__init__(players=players, table=table, checker=checker, game_state=game_state)
+        verify_players_instance(players)
+        verify_table_instance(table)
+        verify_checker_instance(checker)
+        super().__init__(players=players, table=table, game_state=game_state)
+
+        self._checker = checker
+        self._ai = ai
+
+    def step(self, index_row: int, index_column: int, player: PlayerBase):
+        self.table.set_symbol_cell(index_row=index_row, index_column=index_column, symbol=player.symbol)
+        player.add_count_step()
 
     def result(self, player: PlayerBase) -> GameStateT:
         if player.count_steps >= self.table.param.COMBINATION:
 
-            if win_comb := self._win_result(player=player):
+            if win_comb := self._get_win_comb(player=player):
                 self.set_winner(player=player, win_combination=win_comb)
 
-            elif self._draw_result():
+            elif self._get_draw_result():
                 self.set_draw()
         return self.game_state
 
     def step_result(self, index_row: int, index_column: int, player: PlayerBase) -> GameStateT:
-        """
-        Sets the symbol for the given player by the given indices and returns its result \n
-        This is a unifying function. It includes:\n
-        * def self.step
-        * def self.result
-
-        :return GameStateT
-        """
         self.step(index_row=index_row, index_column=index_column, player=player)
         return self.result(player=player)
 
-    def step_result_next_player(self, index_row: int, index_column: int) -> GameStateT:
-        """
-        * The symbol of the next player in queue self.players is placed according to the transmitted indices
-        * +1 move is added to the player.count_step
-        * This player is now available in self.current_player
+    def ai_get_step(self, player: PlayerBase) -> CellIndex:
+        return self._ai.get_step(symbol=player.symbol, table=self.game_field, combinations=self.table.combinations)
 
-        This is a unifying function. It includes:\n
-        * def self.players.set_next_player
-        * def self.step_result → GameStateT
+    def ai_step(self, player: PlayerBase):
+        i_row, i_column = self.ai_get_step(player=player)
+        self.step(index_row=i_row, index_column=i_column, player=player)
 
-        Warning: if you use self.step or self.step_result
-        and pass the argument there players not from the self.players queue,
-        but at your discretion, then this method will determine the next player incorrectly,
-        and most likely, it will return the first player in the queue.\n
-        To avoid this — always use this method, or send players taken from the queue in self.players\n
-        Example: self.players.current_player / self.players.set_next_player
+    def ai_step_result(self, player: PlayerBase) -> GameStateT:
+        i_row, i_column = self.ai_get_step(player=player)
+        return self.step_result(index_row=i_row, index_column=i_column, player=player)
 
-        :return GameStateT
-        """
-        player_now = self.players.set_next_player()
-        return self.step_result(index_row=index_row, index_column=index_column, player=player_now)
+    def set_winner(self, player: PlayerBase, win_combination: CombType):
+        self.game_state.update(code=ResultCode.WINNER, win_player=player, win_combination=win_combination)
+
+    def set_draw(self):
+        self.game_state.update(code=ResultCode.ALL_CELLS_USED)
+
+    def set_get_next_player(self) -> PlayerT:
+        return self._players.set_get_next_player()
+
+    def _get_win_comb(self, player: PlayerBase) -> Optional[CombType]:
+        return self._checker.result_player(player.symbol,
+                                           table=self.table.game_field,
+                                           combinations=self.table.combinations)
+
+    def _get_draw_result(self) -> bool:
+        if self.table.count_free_cells == 0:
+            return True
