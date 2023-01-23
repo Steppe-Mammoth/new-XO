93b136fca3128df028c4fe74527c59fbf71ceda5
Andrew <qwqwqww505@yahoo.com>
2023-01-23 04:51:52 +0200

    Move code in block __name__ == "__main__"

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from game import TableParam, TableDefault, GameConsole, Player, Players, Symbol
 
-p1 = Player(name="ANDROID:1", symbol=Symbol('X'), role=Player.Role.ANDROID)
-p2 = Player(name="ANDROID:4", symbol=Symbol('I'), role=Player.Role.ANDROID)
-p3 = Player(name="ANDROID:3", symbol=Symbol('K'), role=Player.Role.ANDROID)
+if __name__ == "__main__":
+    p1 = Player(name="ANDROID:1", symbol=Symbol('X'), role=Player.Role.ANDROID)
+    p2 = Player(name="ANDROID:4", symbol=Symbol('I'), role=Player.Role.ANDROID)
+    p3 = Player(name="ANDROID:3", symbol=Symbol('K'), role=Player.Role.ANDROID)
 
-p4 = Player(name="PLAYER:2", symbol=Symbol('O'), role=Player.Role.USER)
+    p4 = Player(name="PLAYER:2", symbol=Symbol('O'), role=Player.Role.USER)
 
-players = Players(players=[p1, p2, p3, p4])
-table = TableDefault(param=TableParam(10, 10, 7))
+    players = Players(players=[p1, p2, p3])
+    table = TableDefault(param=TableParam(10, 10, 7))
 
-game = GameConsole(players=players, table=table)
-game.start_game()
+    game = GameConsole(players=players, table=table)
+    game.start_game()
